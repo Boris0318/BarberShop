bd3d31d7c786483a82979637b8926244286db6fc
Boris0318 <borisesgal@gmail.com>
2026-03-23 12:36:04 -0600

    bravado

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@
 // ===================================
 const BARBER_CONFIG = {
     // Business Name
-    businessName: 'Barberia Bisson',
+    businessName: 'Barberia Bravado',
     
     // Contact Information
     phone: '(555) 123-4567',
